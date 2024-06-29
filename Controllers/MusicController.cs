358259ab9ef5b8f487e015c41a77353e079e908d
Pedrozle <pedrozle@outlook.com>
2024-06-28 20:53:51 -0400

    wip: MusicController

---
+++ Controllers/MusicController.cs
@@ -0,0 +1,22 @@
+namespace Spotifried.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Spotifried.Models;
+
+public class MusicController : Controller{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult AddMusic()
+    {
+        return View();
+    }
+
+    public IActionResult RemoveMusic()
+    {
+        return View();
+    }
+
+}
